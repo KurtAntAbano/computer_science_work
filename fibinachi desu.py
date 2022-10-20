e2077e553ea750ad20ff690df60d12205fa7eadb
KurtAntAbano <ka04122005@gmail.com>
2022-10-20 16:30:03 +0100

    yes

--- fibinachi desu.py
+++ fibinachi desu.py
@@ -1,8 +1,11 @@
 count_input = int(input("How many terms would you like"))
-
-def fib_sequence:
-    x = 0
-    y = 1
-
+x, y, count = 0, 1, 0
+fib_list = [0]
+while count_input != count:
     next_num = x + y
+    x = y
     y = next_num
+    count += 1
+    fib_list.append(next_num)
+
+print("Fibonacci sequence =",fib_list)
