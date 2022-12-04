c563f18c9f08a2f8c5cfa9135d5e5632de9c67ae
KurtAntAbano <ka04122005@gmail.com>
2022-12-04 19:26:57 +0000

    yes

--- fibinachi desu.py
+++ fibinachi desu.py
@@ -1,8 +1,13 @@
 count_input = int(input("How many terms would you like"))
+x = 0
+y = 1
 
-def fib_sequence:
-    x = 0
-    y = 1
-
+list = []
+for i in range(0, count_input):
     next_num = x + y
+    x = y
     y = next_num
+
+    list.append(y)
+print(list)
+
