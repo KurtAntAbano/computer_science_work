90729854e42abdba3e23b79ce389008e191eb990
KurtAntAbano <116030268+KurtAntAbano@users.noreply.github.com>
2022-12-05 11:01:48 +0000

    Update Connect four.py

--- Connect four.py
+++ Connect four.py
@@ -1,3 +1,4 @@
+
 board = [["O", "O", "O", "O", "O", "O", "O"],
          ["O", "O", "O", "O", "O", "O", "O"],
          ["O", "O", "O", "O", "O", "O", "O"],
@@ -69,17 +70,17 @@ def update_board(x, colour):
 
 def is_winner(colour):
     for i in range(0,len(board)):
-        for l in range(0,len(board)-1):
+        for l in range(0, 4):
             if board[i][l] == colour and board[i][l + 1] == colour and board[i][l + 2] == colour and board[i][l + 3] == colour:
                 return True
 
-    for i in range(0,len(board)):
-        for l in range(0,len(board)-1):
+    for i in range(0,5):
+        for l in range(0, len(board) - 1 ):
             if board[i][l] == colour and board[i + 1][l] == colour and board[i + 2][l] == colour and board[i + 3][l] == colour:
                 return True
 
-    # for i in range(0,len(board)):
-    #     for l in range(0,len(board)-1):
+    # for i in range(0,5):
+    #     for l in range(0,4):
     #         if board[i][l] == colour and board[i + 1][l + 1] == colour and board[i + 2][l + 2] == colour and board[i + 3][l + 3] == colour:
     #             return True
 
