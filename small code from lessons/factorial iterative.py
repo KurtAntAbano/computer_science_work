bcef4550de1a59fe77f8ec5d0b8296840b90039f
KurtAntAbano <ka04122005@gmail.com>
2022-12-15 11:10:27 +0000

    l

---
+++ small code from lessons/factorial iterative.py
@@ -0,0 +1,19 @@
+def factorial (n):
+    if n ==0:
+        return 1
+    else:
+        return (n*factorial(n-1))
+
+
+
+
+def factorial_it(n):
+    count = 1
+    while n > 1:
+        count *= n
+        n -= 1
+    return count
+
+
+print(factorial_it(8))
+print(factorial(8))
