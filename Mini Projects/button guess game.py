c639dc4b068db8568fd50f373aa40789afdef327
KurtAntAbano <ka04122005@gmail.com>
2022-12-28 20:47:31 +0000

    update

--- Mini Projects/button guess game.py
+++ Mini Projects/button guess game.py
@@ -1,24 +1,30 @@
 import tkinter as tk
 import random
 
-options = ["safe", "wrong"]
+
+options = random.randint(1,3)
+if options == 1:
+    layout = ["lose", "lose", "win"]
+elif options == 2:
+    layout = ["lose", "win", "lose"]
+elif options == 3:
+    layout = ["win", "lose", "win"]
 
 
-def check():
-    check = random.choice(options)
-    if check == "safe":
-        print("safe")
-    if check == "wrong":
-        print("you die")
+def check(b):
+    if b == "win":
+        print("u win")
+    elif b == "lose":
+        print("u lose")
 
 
 window = tk.Tk()
 
-button1 = tk.Button(window, text = "1",command = check)
+button1 = tk.Button(window, text = "1",command = lambda:check(layout[0]))
 button1.pack()
-button2 = tk.Button(window, text = "2",command = check)
+button2 = tk.Button(window, text = "2",command = lambda: check(layout[1]))
 button2.pack()
-button3 = tk.Button(window, text = "3",command = check)
+button3 = tk.Button(window, text = "3",command = lambda: check(layout[2]))
 button3.pack()
 
 window.mainloop()
