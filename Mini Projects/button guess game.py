74fcf37548bdff5123977db2c7a03c7ce9c53db6
KurtAntAbano <ka04122005@gmail.com>
2022-12-30 13:57:55 +0000

    update

--- Mini Projects/button guess game.py
+++ Mini Projects/button guess game.py
@@ -1,30 +1,44 @@
 import tkinter as tk
 import random
 
-
-options = random.randint(1,3)
-if options == 1:
-    layout = ["lose", "lose", "win"]
-elif options == 2:
-    layout = ["lose", "win", "lose"]
-elif options == 3:
-    layout = ["win", "lose", "win"]
-
-
-def check(b):
+def check(b,window):
     if b == "win":
         print("u win")
     elif b == "lose":
         print("u lose")
+    window.destroy()
 
 
-window = tk.Tk()
+def again():
+    continue_win = tk.Tk()
+    button1 = tk.Button(continue_win, text="i would like to continue", command= lambda:[continue_win.destroy(),main()])
+    button1.pack()
+    button2 = tk.Button(continue_win, text="i would like to leave", command=exit)
+    button2.pack()
 
-button1 = tk.Button(window, text = "1",command = lambda:check(layout[0]))
-button1.pack()
-button2 = tk.Button(window, text = "2",command = lambda: check(layout[1]))
-button2.pack()
-button3 = tk.Button(window, text = "3",command = lambda: check(layout[2]))
-button3.pack()
+    continue_win.mainloop()
 
-window.mainloop()
+
+def main():
+    options = random.randint(1,3)
+    if options == 1:
+        layout = ["lose", "lose", "win"]
+    elif options == 2:
+        layout = ["lose", "win", "lose"]
+    elif options == 3:
+        layout = ["win", "lose", "win"]
+
+    window = tk.Tk()
+
+    button1 = tk.Button(window, text="1", command=lambda: check(layout[0],window))
+    button1.pack()
+    button2 = tk.Button(window, text="2", command=lambda: check(layout[1],window))
+    button2.pack()
+    button3 = tk.Button(window, text="3", command=lambda: check(layout[2],window))
+    button3.pack()
+
+    window.mainloop()
+    again()
+
+
+main()
