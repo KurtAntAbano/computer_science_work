5743185ae9107feafae17eb9adacff2ee4177363
KurtAntAbano <ka04122005@gmail.com>
2023-02-01 18:54:32 +0000

    update

--- homework/fridge.py
+++
@@ -1,8 +0,0 @@
-for x in range(0,len(words))
-    for i in range(0, len(words)):
-        for l in range(0, 4):
-            if display[i][l] == "":
-                display[i][l] = word[x]
-
-
-
---
+++ homework/pi calculate with formula.py
@@ -0,0 +1,5 @@
+import math
+x = int(input("input a large number"))
+y = 180/x
+pi = x * math.sin(y)
+print(pi)
