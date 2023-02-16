0b300b93f502ef52ea97c1756b6342b1581fea15
KurtAntAbano <ka04122005@gmail.com>
2023-02-16 20:09:56 +0000

    update

---
+++ homework/linear search.py
@@ -0,0 +1,14 @@
+def linear_search(data_set, item_sought):
+    index = -1
+    i = 0
+    found = False
+    while i < len(data_set) and found == False:
+        if data_set[i] == item_sought:
+            index = i
+            found = True
+        i = i + 1
+    return index
+
+print(linear_search([200,12,34,543,342,55,800], 543))
+
+
