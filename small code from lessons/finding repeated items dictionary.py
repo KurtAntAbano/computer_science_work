d9fcd106516fbbbfa02453c5a9d1c92356af0425
KurtAntAbano <ka04122005@gmail.com>
2023-02-28 17:16:16 +0000

    update

--- small code from lessons/finding repeated items dictionary.py
+++ small code from lessons/finding repeated items dictionary.py
@@ -7,28 +7,16 @@
 def repeat_search(data_set):
     dictionary = {}
     for i in range(0, len(data_set)):
+        count = 0
         item = data_set[i]
+
         if item not in dictionary.keys():
             for j in range(0, len(data_set)):
-                count = 0
                 if data_set[j] == item:
                     count += 1
             dictionary[item] = count
 
-
     print(dictionary)
 
 
-
-    # for j in range(0, len(data_set)):
-    #     count = 0
-    #     item = data_set[j]
-    #     for g in range(0, len(data_set)):
-    #         if data_set[g] == item:
-    #             count += 1
-
-
-
-
-
-repeat_search([2, 2, 3, 4, 4 , 3, 2, 7])
+repeat_search([2, 2, 3, 4, 4, 3, 2, 7, 8, 8, 8, 8, 8])
