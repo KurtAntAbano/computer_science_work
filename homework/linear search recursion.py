41601a016769e60ff968964c9bef4a8091702228
KurtAntAbano <ka04122005@gmail.com>
2023-03-06 18:56:58 +0000

    update

---
+++ homework/linear search recursion.py
@@ -0,0 +1,10 @@
+def linear_search(data_set, item_sought, i):
+    if data_set[i] == item_sought:
+        return i
+    elif i == len(data_set)-1:
+        return -1
+    else:
+        return linear_search(data_set, item_sought, i + 1)
+
+
+print(linear_search([12, 23, 55, 65, 3, 4, 59, 100], 900, 0))
