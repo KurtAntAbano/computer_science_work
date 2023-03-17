a2a6919e218a370526c76cf6f0ccaf0d09c4a3b3
KurtAntAbano <ka04122005@gmail.com>
2023-03-17 15:43:32 +0000

    update

--- homework/linear search.py
+++ homework/linear search.py
@@ -6,7 +6,7 @@ def linear_search(data_set, item_sought):
         if data_set[i] == item_sought:
             index = i
             found = True
-        i = i + 1
+        i += 1
     return index
 
 
