bae42fa499de9aa5168a882cb9d14ef93506e2c6
KurtAntAbano <ka04122005@gmail.com>
2023-04-17 16:52:07 +0100

    update

--- independent learning programming red booklet/q1 cards 2007 .py
+++ independent learning programming red booklet/q1 cards 2007 .py
@@ -3,10 +3,19 @@ def sum15(list):
     for i in range(0, len(list)):
         for j in range(i, len(list)):
             sum = 0
-            for k in range(i, j+1):
+            for k in range(i, j + 1):
                 sum = sum + list[k]
             if sum == 15:
                 score += 1
+
+    for i in range(4, 0, -1):
+        for j in range(4, i, -1):
+            sum = 0
+            for k in range(j , i - 1, -1):
+                sum = sum + list[k]
+            if sum == 15:
+                score += 1
+
     return score
 
 
@@ -21,11 +30,12 @@ def duplicate(list):
             for j in range(0, len(list)):
                 if list[j] == item:
                     count += 1
-            if count > 1:
+            if count == 2:
+                score += 1
+            elif count > 1 and count != 2:
                 score += count
             checked.append(item)
     return score
-
 
 
 def main():
@@ -35,5 +45,6 @@ def main():
         list.append(x)
 
     print(sum15(list) + duplicate(list))
+
 
 main()
