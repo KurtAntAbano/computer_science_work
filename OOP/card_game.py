563c2f7e00953ad09d57b8ce6e0f5947473c2b56
KurtAntAbano <ka04122005@gmail.com>
2023-04-25 12:30:41 +0100

    update

---
+++ OOP/card_game.py
@@ -0,0 +1,26 @@
+class Player:
+    def __init__(self, given_name):
+        self.__name = given_name
+        self.__score = 0
+
+#  create card list
+#  jumble the order of the card list
+#  when dealing the new jumbled list, deal the first card of the list
+
+
+
+class Deck:
+
+    def __init__(self):
+        self.__cards = []
+        # Code to generate a deck of cards here
+
+
+    def shuffle(self):
+        # Code to shuffle cards here
+        pass
+
+
+    def deal(self):
+        # Code to deal a card from the deck
+        pass
