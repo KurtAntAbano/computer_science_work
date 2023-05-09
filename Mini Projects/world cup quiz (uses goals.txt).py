1028e846700766514feef8347d29fe729699bca7
KurtAntAbano <ka04122005@gmail.com>
2023-05-09 20:17:50 +0100

    update

--- Mini Projects/world cup quiz (uses goals.txt).py
+++ Mini Projects/world cup quiz (uses goals.txt).py
@@ -1,4 +1,8 @@
 import random
+from tkinter import *
+
+from future.moves.tkinter import messagebox
+
 
 def country_goal_list():
     countries = []
@@ -12,7 +16,8 @@ def country_goal_list():
             countries.append(country)
 
     file.close()
-    main(countries)
+    mainMenuGUI(countries)
+
 
 def how_many_goals(country):
     file = open("goals.txt", "r")
@@ -28,12 +33,37 @@ def how_many_goals(country):
     return count
 
 
+def mainMenuGUI(countries):
+    # code for the main menu
+    # from this window, I can go to log in or signin or exit
 
-def main(countries):
     randomCountry1 = random.choice(countries)
     randomCountry2 = random.choice(countries)
 
-    whichCountry = int(input(f"Which country do u think scored more goals? {randomCountry1} or {randomCountry2} (1 or 2)"))
+    win1 = Tk()
+    win1.title("Welcome")
+    win1.geometry("400x180")
+
+    titleLabel = Label(win1, text=f"Which country do u think scored more goals? {randomCountry1} or {randomCountry2}")
+    titleLabel.grid(row=0, column=0, columnspan=3, sticky="SNEW", pady=10, padx=10)
+
+    exitButton = Button(win1, text="Exit", width=12, command=quit)
+    exitButton.grid(row=1, column=0, padx=10, pady=30)
+
+    bt_country1 = Button(win1, text=f"{randomCountry1}", width=12,
+                         command=lambda: has_won(win1, randomCountry1, randomCountry1, randomCountry2))
+    bt_country1.grid(row=1, column=1, padx=10, pady=10)
+
+    bt_country1 = Button(win1, text=f"{randomCountry2}", width=12,
+                         command=lambda: has_won(win1, randomCountry2, randomCountry1, randomCountry2))
+    bt_country1.grid(row=1, column=2, padx=10, pady=10)
+
+    mainloop()
+
+
+def has_won(w, country, randomCountry1, randomCountry2):
+    w.destroy()
+
     Country1_goals = how_many_goals(randomCountry1)
     Country2_goals = how_many_goals(randomCountry2)
 
@@ -42,12 +72,13 @@ def main(countries):
     else:
         answer = 2
 
-    if whichCountry == answer:
-        print("\nWell done, you are correct")
+    if country == answer:
+        messagebox.showinfo(title="Success", message=(f"U ARE CORRECT!\n{randomCountry1} scored {Country1_goals},"
+                                                      f" {randomCountry2} scored {Country2_goals}"))
     else:
-        print("\nsorry ur wrong")
+        messagebox.showinfo(title="ERROR", message=(f"U ARE WRONG!\n{randomCountry1} scored {Country1_goals},"
+                                                    f" {randomCountry2} scored {Country2_goals}"))
+    country_goal_list()
 
-    print(f"\n{randomCountry1} scored {Country1_goals}, {randomCountry2} scored {Country2_goals}")
 
 country_goal_list()
-
