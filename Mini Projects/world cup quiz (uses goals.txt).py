fcd7da6169ee62494d9b874ae809a2a7ab69b9a5
KurtAntAbano <ka04122005@gmail.com>
2023-05-10 19:20:04 +0100

    update

--- Mini Projects/world cup quiz (uses goals.txt).py
+++ Mini Projects/world cup quiz (uses goals.txt).py
@@ -33,6 +33,11 @@ def how_many_goals(country):
     return count
 
 
+def dark_change(w):
+        w.configure(bg="dark grey")
+
+def light_change(w):
+        w.configure(bg="light grey")
 def mainMenuGUI(countries):
     # code for the main menu
     # from this window, I can go to log in or signin or exit
@@ -43,6 +48,7 @@ def mainMenuGUI(countries):
     win1 = Tk()
     win1.title("Welcome")
     win1.geometry("400x180")
+    win1.configure(bg="light grey")
 
     titleLabel = Label(win1, text=f"Which country do u think scored more goals? {randomCountry1} or {randomCountry2}")
     titleLabel.grid(row=0, column=0, columnspan=3, sticky="SNEW", pady=10, padx=10)
@@ -51,12 +57,18 @@ def mainMenuGUI(countries):
     exitButton.grid(row=1, column=0, padx=10, pady=30)
 
     bt_country1 = Button(win1, text=f"{randomCountry1}", width=12,
-                         command=lambda: has_won(win1, randomCountry1, randomCountry1, randomCountry2))
+                         command=lambda: has_won(win1, 1, randomCountry1, randomCountry2))
     bt_country1.grid(row=1, column=1, padx=10, pady=10)
 
     bt_country1 = Button(win1, text=f"{randomCountry2}", width=12,
-                         command=lambda: has_won(win1, randomCountry2, randomCountry1, randomCountry2))
+                         command=lambda: has_won(win1, 2, randomCountry1, randomCountry2))
     bt_country1.grid(row=1, column=2, padx=10, pady=10)
+
+    bt_themeChange = Button(win1, text=f"Dark mode", width=12, command=lambda: dark_change(win1))
+    bt_themeChange.grid(row=2, column=2, padx=10, pady=10)
+
+    bt_themeChange = Button(win1, text=f"Dark mode", width=12, command=lambda: light_change(win1))
+    bt_themeChange.grid(row=2, column=0, padx=10, pady=10)
 
     mainloop()
 
