a3218b4b32740dd1b8167e3622ac680670c209fe
KurtAntAbano <ka04122005@gmail.com>
2023-05-18 11:54:51 +0100

    update

--- SQL work/SQL testing.py
+++ SQL work/SQL testing.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def createTable():
     connector = sqlite3.connect("test.db")
     print("Opened database successfully")
@@ -14,5 +15,22 @@ def createTable():
     print("Table created successfully")
     connector.close()
 
+
+def insertData():
+    connector = sqlite3.connect("test.db")
+    connector.execute("INSERT INTO COMPANY (staffID,name,room,address,salary)"
+                      "VALUES (1, 'Paul', 32, 'STOKE', 20000.00)");
+
+    connector.execute("INSERT INTO COMPANY (staffID,name,room,address,salary)"
+                      "VALUES (123, 'Kurt', 44, 'STOKE', 20000.00)");
+
+    connector.commit()
+    print("Records created successfully")
+    connector.close()
+
+
 if __name__ == "__main__":
     createTable()
+    insertData()
+
+#  Use SQLITE3 viewer, view the database file, you should be able to see the records
